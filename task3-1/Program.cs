ffdc4316a510e1f303bdca9a718fba8ab300f247
Nastya_keks <aagolunova@edu.hse.ru>
2022-09-26 15:05:09 +0300

    task1

---
+++ task3-1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine());
+
+int first = N / 10000;
+int second = N/1000 % 10;
+int third = N/100 % 10;
+int forth = N/10 % 10;
+int five = N % 10;
+
+if (first == five & second == forth)
+{
+    Console.Write(N);
+    Console.WriteLine("-> да");
+}
+else
+{Console.Write(N);
+    Console.WriteLine("-> нет");
+}
